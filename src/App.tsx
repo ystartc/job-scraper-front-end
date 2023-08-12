220df25fdaf92e6299cc66c4708d9de97b29b5ef
ystartc <startceva.dev@gmail.com>
2023-08-11 18:05:11 -0700

    Added basic structure.

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <section className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Header</p>
       </header>
-    </div>
+      <main className='App-main'>
+        <p>Main Body</p>
+      </main>
+      <footer className='App-footer'>
+        <p>Footer</p>
+      </footer>
+    </section>
   );
 }
 
