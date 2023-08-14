b61108b01ee115128315af354337f0ac855e98ea
ystartc <startceva.dev@gmail.com>
2023-08-14 00:24:22 -0700

    Css add-ons.

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,17 +1,17 @@
 const MainPage: React.FC  = () => {
   return (
-    <section className="homepage">
-      <header className="welcome">
+    <section className='homepage'>
+      <header className='welcome'>
         <h1>Welcome to Job Scraper!</h1>
         <img src='https://www.pharmiweb.jobs/_/jobseekers-frontend/image/77dbe175-0b9c-498b-8d74-c4730262f626' alt='Scraping'></img>
-        <p>Find the best job opportunities that match your skills and preferences.</p>
       </header>
-      <main className="features">
-        <section className="feature">
+      <main className='features'>
+        <section className='feature'>
+        <p>Find the best job opportunities that match your skills and preferences.</p>
           <h2>Easy Job Search</h2>
           <p>Search and filter job postings from various sources to find the perfect match for you.</p>
         </section>
-        <section className="feature">
+        <section className='feature'>
           <h2>Stay Updated</h2>
           <p>Get real-time updates on new job postings that match your preferences.</p>
         </section>
