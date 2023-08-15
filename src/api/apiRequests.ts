adf735be8e9294fea98f0b7aa2a0ed2be0039718
ystartc <startceva.dev@gmail.com>
2023-08-15 13:22:12 -0700

    Added default route and description.

--- src/api/apiRequests.ts
+++ src/api/apiRequests.ts
@@ -3,7 +3,7 @@ import { IJob } from '../interfaces/job';
 
 export const loadJobs = () => {
   return (
-    axios.get<IJob[]>('https://job-scraper-api.fly.dev/jobs')
+    axios.get<IJob[]>('https://job-scraper-api.fly.dev/jobs?title=software')
     .then((response: any) => {
       const jobs = response.data.map((job: any) => {
         job.jobId = job.job_id;
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,8 +3,14 @@ import SearchForm from '../components/SearchForm';
 const SearchPage = () => {
   return (
     <section className='search-page'>
-      <p className='title-search'>What are you looking for?</p>
+      <h1 className='title-search'>What are you looking for?</h1>
       <SearchForm/>
+      <section className='project-warning'>
+        <h2>OnGoing project!!!</h2>
+        <p>In light of the ongoing development work on the project, it is strongly recommend to use the search button without providing any input in the search field.</p>
+        <p>Serch button prompts to Software Engineer jobs in Seattle area within 50 miles</p>
+        <p>Have fun! 😉</p>
+      </section>
     </section>
   )
 }
